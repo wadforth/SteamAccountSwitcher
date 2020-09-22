54ef46d9e2f5de10755dc18e18ba72d49b71932a
Kieran <45768489+wadforth@users.noreply.github.com>
2020-09-22 11:24:49 +0100

    added new/delete users, fixed looping

--- Steam Switch Accounts.bat
+++ Steam Switch Accounts.bat
@@ -1,10 +1,9 @@
 @echo off
 color 70
 mode con: cols=50 lines=10
+setlocal
+cd %localappdata%
 TITLE Steam Account Switcher
-
-setlocal EnableDelayedExpansion
-setlocal enabledelayedexpansion
 
 :steamProcess
 echo 		Process Check
@@ -13,27 +12,32 @@ timeout 3 >nul
 tasklist | find /i "steam.exe" && echo steam.exe is running  && powershell -Command Write-Host "terminating process..." -foreground "red" && timeout 3 >nul && taskkill /im steam.exe /F || echo( && echo process "steam.exe" not running. && loading... && timeout 2 >nul
 cls
 
+:folder
+if not exist SteamAccountSwitcher ( 
+md SteamAccountSwitcher  && cd SteamAccountSwitcher && GOTO accountSetup
+) else ( cd SteamAccountSwitcher && GOTO configFile)
+
 :configFile
-if exist %localappdata%\SteamAccountSwitcher\config.txt  (
-< %localappdata%\SteamAccountSwitcher\config.txt (
+if exist config.txt  (
+< config.txt (
   set /p mainuser=
   set /p altuser=
 ) && GOTO selectionScreen) else (
 GOTO accountSetup )
 
 :accountSetup
+break>config.txt
 powershell -Command Write-Host "/   Main Account Set-up" -foreground "blue"
 set /p "mainuser=Username: " 
-echo %mainuser% >> %localappdata%\SteamAccountSwitcher\config.txt
+echo %mainuser% >> config.txt
 cls
 powershell -Command Write-Host "/   Alt Account Set-up" -foreground "blue"
 set /p "altuser=Username: "
-echo %altuser% >> %localappdata%\SteamAccountSwitcher\config.txt
-
-cls
+echo %altuser% >> config.txt
 
 :selectionScreen
 mode con: cols=56 lines=20
+cls
 echo(
 echo	========================================================
 echo 		Select your account
@@ -41,6 +45,9 @@ echo ========================================================
 echo(
 echo 1 - %mainuser%
 echo 2 - %altuser%
+echo(
+echo 3 - Edit Users
+echo 4 - Delete Data
 echo(
 echo(
 echo Script currently only supports two accounts.
@@ -50,20 +57,29 @@ echo(
 echo(
 echo(
 echo(
-echo(
-CHOICE /M Select /C 12
+CHOICE /C 1234 
 
-If Errorlevel 2 Goto 2
-If Errorlevel 1 Goto 1
+IF ERRORLEVEL 4 GOTO 4
+IF ERRORLEVEL 3 GOTO 3
+IF ERRORLEVEL 2 GOTO 2
+IF ERRORLEVEL 1 GOTO 1
 
+:4
+cd ..
+RD /S /Q "SteamAccountSwitcher" 
+cls
+echo Data has been deleted.  Exiting...
+timeout 3 >nul
+exit
+:3
+cls
+GOTO accountSetup
 :2
 set username=%altuser%
-Goto end
+GOTO end
 :1
 set username=%mainuser%
-Goto end
-
-
+GOTO end
 
 :end
 reg add "HKCU\Software\Valve\Steam" /v AutoLoginUser /t REG_SZ /d %username% /f
